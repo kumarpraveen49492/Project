a5d9453ad8d6d1d96d8269e4b75f831fb531bcd6
Praveen <kumarpraveen49492@gmail.com>
2026-01-16 16:12:12 +0530

    Add FastAPI product API

---
+++ product_api.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+app=FastAPI()
+class Product(BaseModel):
+    id:int
+    name:str
+    description:str
+    price:float
+products = [
+   Product(id=1,name="Phone", description="A Tablet Phones",price=688.9),
+   Product(id=2,name="Laptop", description="Asus laptop",price=888.9),
+   Product(id=3,name="vehicles", description="A motor bike",price=1988.9),
+   Product(id=4,name="shooes", description="A puma shoes",price=288.9)
+]
+@app.get("/products")
+
+def get_all_products():
+    return products
+@app.get("/products/{id}")
+def product_get_all(id:int):
+    for product in products:
+        if product.id==id:
+            return product
+    return {"error":"products not found"}
+@app.post("/products")
+def post_products(Product:Product):
+    products.append(Product)
+    return Product
+
+
+
+
+
